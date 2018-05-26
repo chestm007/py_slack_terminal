8a18e44574b2919dbe2f513ee4e3379295b5edf0
Max <chestm007@hotmail.com>
2018-05-26 15:48:38 +1000

    much cleaner RTM client implementation

--- lib/slack_client/rtmclient.py
+++ lib/slack_client/rtmclient.py
@@ -1,49 +1,25 @@
 import json
 
-from threading import Thread
-from websocket import create_connection
-from lib.logger import Logger
+import websocket
+import threading
 
 
 class SlackRTMClient:
     def __init__(self, url, callback):
-        self.logger = Logger('SlackRTMClient')
-        self.url = url
-        self._continue = True
-        self.thread = Thread(target=self.event_loop)
-        self.ws = None
         self.callback = callback
-
-    def event_loop(self):
-        while self._continue:
-            try:
-                result = self.ws.recv()
-                response = json.loads(result)
-                self.callback(response)
-
-            except ConnectionResetError:
-                self.reconnect()
-
-            except Exception as e:
-                self.logger.log(e + result)
-                pass
-
-    def reconnect(self):
-        self.disconnect()
-        self.connect()
-
-    def connect(self):
-        self.ws = create_connection(self.url)
-
-    def disconnect(self):
-        self.ws.close()
+        self.ws = websocket.WebSocketApp(url, on_message=self.on_message)
+        self.wst = threading.Thread(target=self.ws.run_forever)
+        self.wst.daemon = True
 
     def start(self):
-        self.connect()
-        self._continue = True
-        self.thread.start()
+        self.wst.start()
+
+    def on_message(self, _, message):
+        data = json.loads(message)
+        self.callback(data)
 
     def stop(self):
-        self._continue = False
-        self.thread.join()
-        self.disconnect()
+        self.ws.close()
+        self.wst.join(timeout=10)
+
+
