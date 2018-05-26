37e039abb5f942b63bc9b1412695657331ddb2d5
Max <chestm007@hotmail.com>
2018-05-26 15:54:11 +1000

    create config file if not exists.

--- lib/config.py
+++ lib/config.py
@@ -3,8 +3,20 @@ import yaml
 
 
 class Config:
+    config_path = os.path.expanduser('~') + '/.config/py_slack_term/'
+    config_filename = 'config.yml'
+
     def __init__(self):
-        with open(os.path.expanduser('~') + '/.config/py_slack_term/config.yml') as config_file:
+        if not os.path.exists(self.config_path):
+            os.mkdir(self.config_path)
+        if not os.path.isfile(self.config_path + self.config_filename):
+            with open(self.config_path + self.config_filename, 'w+') as config_file:
+                config = dict(
+                    slacktoken=input("Please paste your slack API token: ")
+                )
+                config_file.write(yaml.dump(config))
+
+        with open(self.config_path + self.config_filename) as config_file:
             config = yaml.load(config_file)
         self.token = config.get('slacktoken')
 
