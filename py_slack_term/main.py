d11d3b85017d739948d948deb89ec55e809a9cb9
Max <chestm007@hotmail.com>
2018-05-27 14:28:13 +1000

    implement argparser and add version flag

--- py_slack_term/main.py
+++ py_slack_term/main.py
@@ -1,18 +1,29 @@
+import pkg_resources
+
 from .lib import Config
 from .lib.UI import SlackApplication
 
 from py_slack_term.lib.slack_client.API import SlackApiClient
 
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('--version', action='store_true')
+args = parser.parse_args()
+
 
 def main():
-    config = Config()
-    slackclient = SlackApiClient(config)
-    app = SlackApplication(slack_client=slackclient)
-    try:
-        app.run()
-    except KeyboardInterrupt:
-        app.stop()
-        pass
+    if args.version:
+        version = pkg_resources.get_distribution("py_slack_term").version
+        print("\nVersion: py_slack_term: {}".format(version))
+    else:
+        config = Config()
+        slackclient = SlackApiClient(config)
+        app = SlackApplication(slack_client=slackclient)
+        try:
+            app.run()
+        except KeyboardInterrupt:
+            app.stop()
+            pass
 
 if __name__ == "__main__":
     main()
